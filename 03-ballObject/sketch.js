295298301ce9ec05fbddec0ac2b7126076d51d0a
LllimaBeans <146811819+LllimaBeans@users.noreply.github.com>
2023-10-06 10:38:05 -0600

    Refactored ball object code

--- 03-ballObject/sketch.js
+++ 03-ballObject/sketch.js
@@ -2,19 +2,25 @@
 // Laura Lima
 // Oct 5th
 
-let theBall = {
-  x: 100,
-  y: 100,
-  radius: 25,
-  r: 230,
-  g: 20,
-  b: 50,
-  dx: -30,
-  dy: -50,
-};
+let theBall;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  theBall = spawnBall();
+}
+
+function spawnBall() {
+  let theBall = {
+    x: random(width),
+    y: random(height),
+    radius: random(15, 30),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
+    dx: random(-10, 10),
+    dy: random(-10, 10),
+  };
+  return theBall;
 }
 
 function draw() {
@@ -23,25 +29,31 @@ function draw() {
   moveBall();
 }
 
+function keyTyped() {
+  if (key === " ") {
+    theBall = spawnBall();
+  }
+}
+
 function moveBall() {
-  if (theBall.dx < 0) {
-    if (theBall.x < 0) {
-      theBall.x = width - theBall.radius;
-    } 
-  }
-  if (theBall.dy < 0) {
-    if (theBall.y > 0) {
-      theBall.y = height;
-    }
-  }
-  if (theBall.x > width) {
+  if (theBall.x  - theBall.radius > width) {
     theBall.x = 0 - theBall.radius;
   }
-  if (theBall.y > height) {
+  else if (theBall.x < 0 - theBall.radius){
+    theBall.x = width + theBall.radius;
+  }
+
+  if (theBall.y - theBall.radius > height) {
     theBall.y = 0 - theBall.radius;
   }
+  else if (theBall.y < 0 - theBall.radius) {
+    theBall.y = height + theBall.radius;
+  }
+
   theBall.x += theBall.dx;
   theBall.y += theBall.dy;
+
+  console.log(theBall.x, theBall.y);
 }
 
 function displayBall() {
